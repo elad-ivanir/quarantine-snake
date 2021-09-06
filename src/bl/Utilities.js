e7d7fccbb0a106692014f11256423801df184ff8
elad-ivanir <eladivanir64@gmail.com>
2021-09-06 12:33:12 +0300

    initial getAngle function

--- src/bl/Utilities.js
+++ src/bl/Utilities.js
@@ -1,47 +1,31 @@
-// export function getAngle(point1, point2) {
-//   const dY = point2.y - point1.y;
-//   const dX = point2.x - point1.x;
-//   const angle = Math.atan(dX / dY);
-//   const absoluteAngle = angle >= 0 ? angle : angle + Math.PI;
-//   return dX >= 0 ? absoluteAngle : Math.PI + absoluteAngle;
-// } // TODO: function NOT correct
+export function areOpposite(angle1, angle2) {
+  return Math.abs(angle2 - angle1) === Math.PI;
+}
 
-// export function areOpposite(angle1, angle2) {
-//   return Math.abs(angle2 - angle1) === Math.PI;
-// }
+export function getDistantPoint(sourcePoint, distance, angle) {
+  const x = sourcePoint.x + distance * Math.sin(angle);
+  const y = sourcePoint.y + distance * Math.cos(angle);
+  return { x, y };
+} // TODO: check correctness
 
-// export function getDistantPoint(sourcePoint, distance, angle) {
-//   const x = sourcePoint.x + distance * Math.sin(angle);
-//   const y = sourcePoint.y + distance * Math.cos(angle);
-//   return { x, y };
-// } // TODO: check correctness
+export function getDistance(point1, point2) {
+  const dY = point2.y - point1.y;
+  const dX = point2.x - point1.x;
+  return Math.sqrt(dY ** 2 + dX ** 2);
+}
 
-// export function getDistance(point1, point2) {
-//   const dY = point2.y - point1.y;
-//   const dX = point2.x - point1.x;
-//   return Math.sqrt(dY ** 2 + dX ** 2);
-// }
-
-function getAngle(point1, point2) {
+export function getAngle(point1, point2) {
   const dY = point2.y - point1.y;
   const dX = point2.x - point1.x;
   const angle = Math.atan(dX / dY);
+  /* get positive represntation of angle in case of negative */
   const absoluteAngle = angle >= 0 ? angle : angle + Math.PI;
-  return dX >= 0 ? absoluteAngle : Math.PI + absoluteAngle;
+  /* get angle in case of reverse direction, TODO: make cleaner? */
+  if (dX < 0) {
+    return absoluteAngle + Math.PI;
+  } else if (dX === 0) {
+    return dY > 0 ? 0 : Math.PI;
+  } else {
+    return absoluteAngle;
+  }
 }
-
-const test =
-  (getAngle(
-    {
-      x: 4,
-      y: 5,
-    },
-    {
-      x: 4,
-      y: 3,
-    }
-  ) *
-    180) /
-  Math.PI;
-
-console.log(test);
