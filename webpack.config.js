b9e2ffc91abacee050af729688cf00a7bfb72a5e
elad-ivanir <eladivanir64@gmail.com>
2021-09-07 09:57:42 +0300

    webpack html config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  context: path.resolve(__dirname, "./src"),
+  entry: "./main.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "public/build"),
+    path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: "./index.html",
+      filename: "index.html",
+    }),
+  ],
 };
